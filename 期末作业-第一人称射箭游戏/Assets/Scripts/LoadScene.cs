f29682922341e47204055e5187e98abf8b358333
Miao-Da <2315108020@qq.com>
2024-12-12 23:46:21 +0800

    Fix 期末作业-第一人称射箭游戏 folder as a regular directory

---
+++ 期末作业-第一人称射箭游戏/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    //资源加载
+    public void Load(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+    
+}
